0b3f6b054639d59b8529d3097976ca2767b1f024
Sohammj <soham0144@gmail.com>
2026-02-01 23:56:37 +0530

    SSS

--- src/components/showcase/Journey.tsx
+++ src/components/showcase/Journey.tsx
@@ -19,9 +19,9 @@ const Journey: React.FC<JourneyProps> = () => {
         <br />
 
         <p>
-          Today I’m here for two reason:
+          Today I’m here for one reason:
           <br />• To show you the <b>exact system</b> I followed (so you can copy it)
-          <br />• To meet you — because I’ll be <b>joining you next year</b>
+          {/* <br />• To meet you — because I’ll be <b>joining you next year</b> */}
         </p>
 
         <br />
